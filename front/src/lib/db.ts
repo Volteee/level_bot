c9c40552adc2fa6ae1859b3267b80094e6ad3479
Volteee <maksim-kim06@mail.ru>
2025-10-17 00:20:13 +0300

    fix

--- front/src/lib/db.ts
+++ front/src/lib/db.ts
@@ -4,7 +4,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   port: parseInt(process.env.POSTGRES_PORT || '5432'),
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   ssl: true,
