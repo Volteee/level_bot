acd519ffd08d236bb7a71c895d25afa3d708cd4d
Volteee <maksim-kim06@mail.ru>
2025-10-17 00:52:53 +0300

    fix

--- front/next.config.js
+++ front/next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'standalone',
-  basePath: '',
-  webpack(config, { dev, isServer }) {
-    // Required for Hot Reloading on Windows
-    // https://github.com/vercel/next.js/issues/6417
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      }
-    }
-    return config
-  },
-}
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
